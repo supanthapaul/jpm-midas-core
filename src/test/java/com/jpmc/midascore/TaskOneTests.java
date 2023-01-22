97c433210189a759027e986f94ec34b16749c740
Supantha Paul <supapaul56@gmail.com>
2023-01-22 21:35:13 +0530

    Configured dependencies

--- src/test/java/com/jpmc/midascore/TaskOneTests.java
+++ src/test/java/com/jpmc/midascore/TaskOneTests.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import scala.App;
 
-@SpringBootTest
+@SpringBootTest(classes = App.class)
 class TaskOneTests {
     static final Logger logger = LoggerFactory.getLogger(TaskOneTests.class);
 
